338343c97d9216a41fadba6485d63ca938c7d14b
euFilpeSilva <sousadasilvafilipe@gmail.com>
2023-06-11 12:56:17 -0300

    implementação metodo mediaCA

--- src/models/nota.js
+++ src/models/nota.js
@@ -14,9 +14,26 @@ class Nota {
         );
     }
 
-    mediaCA() {
-        return "SS";
+    mediaCA(media) {
+    if (media >= 9.0 && media <= 10) {
+        return 'SS';
+    } else if (media >= 7.0 && media < 9.0) {
+        return 'MS';
+    } else if (media >= 5.0 && media < 7.0) {
+        return 'MM';
+    } else if (media >= 3.0 && media < 5.0) {
+        return 'MI';
+    } else if (media >= 0.1 && media < 3.0) {
+        return 'II';
+    } else if (media === 0) {
+        return 'SR';
+    } else {
+        throw new Error('Média inválida');
     }
+    }
+
+
+
 }
 
 module.exports = Nota;
